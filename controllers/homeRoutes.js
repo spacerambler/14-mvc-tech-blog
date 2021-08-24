bd62ce1db84ab27f8c3a5c80830d1b6f04cffc55
Keely Bailey <keelybailey3@gmail.com>
2021-08-24 17:19:32 -0500

    Add in dashboard homeroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,4 +48,27 @@ router.get('/', (req, res) =>{
 })
 });
 
+router.get('/dashboard', withAuth, (req, res) => {
+    User.findByPk(req.session.user_id, {
+        include: [{ model: Post }]
+    }).then((userData, err) => {
+        if(err)res.status(500).json(err);
+    })
+
+    const posts = userData.map((post) => post.get({ plain: true}));
+
+    res.render('dashboard', {
+        ...user,
+        logged_in: true
+    })
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+})
+
 module.exports = router;
