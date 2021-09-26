7f129e1c2e7f8daa22baeeb66e6af73d37f70bfb
Keely Bailey <keelybailey3@gmail.com>
2021-09-26 16:30:59 -0500

    Restart user routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,73 +1,18 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
-const withAuth = require('../utils/auth.js');
-const sequelize = require('../config/connection.js');
 
-// router.get('/', async (req, res) =>{
-//     try {
-//         const postData = await Post.findAll({
-//             include: [
-//                 {
-//                     model: User,
-//                     attributes: ['name'],
-//                 },
-//             ],
-//         });
+// Create new user
 
-//         const posts = postData.map((post) => post.get({ plain: true }));
+// Login user
 
-//         res.render('homepage', {
-//             posts
-//         })
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+// Logout user
 
-router.get('/', (req, res) =>{
-         Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ],
-        }).then((postData,err)=>{
+// update user
 
-            if(err)res.status(500).json(err);
-        
+// Find all users
 
-            const posts = postData.map((post) => post.get({ plain: true }));
+// find one user
 
-            res.render('homepage', {
-                posts
-            })
-    
-    
-})
-});
-
-router.get('/dashboard', withAuth, (req, res) => {
-    User.findByPk(req.session.user_id, {
-        include: [{ model: Post }]
-    }).then((userData, err) => {
-        if(err)res.status(500).json(err);
-    })
-
-    const posts = userData.map((post) => post.get({ plain: true}));
-
-    res.render('dashboard', {
-        ...user,
-        logged_in: true
-    })
-})
-
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/dashboard');
-        return;
-    }
-    res.render('login');
-})
+// delete user
 
 module.exports = router;
