c72d40b9e9d5ba555c67734fbf8f311c0a1ccb4b
Keely Bailey <keelybailey3@gmail.com>
2021-09-26 20:54:52 -0500

    Add route to get one post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -43,6 +43,34 @@ router.get('/', async (req, res) => {
 })
 
 // get one post
+router.get('/:id', async (req, res) => {
+    try {
+        Post.findOne({
+            attributes: ["id", "title", "postCopy", "date_created"],
+            include: [
+                {
+                    model: User, 
+                    attributes: ["name"],
+                }, 
+                {
+                    model: Comment, 
+                    attributes: ["id", "commentContent", "date_created"], 
+                    include: {
+                        model: User,
+                        attributes: ["name"]
+                        }
+                }
+            ],
+        });
+        if (!userData) {
+            res.status(400).json({ message: 'No user found with that id'});
+            return;
+        };
+        res.status(200).json(postData);
+    } catch(err) {
+        res.status(500).json(err)
+    };
+})
 
 // edit post
 router.put('/dashboard/:id', withAuth, async (req, res)=>{
