8135950dccbe434f9f26f38bf78573b6a5067c1b
Keely Bailey <keelybailey3@gmail.com>
2021-09-26 21:21:49 -0500

    Start new post front end

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes.js')
 
 router.use('/users', userRoutes);
+router.use('/posts', userRoutes);
 
 module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -73,7 +73,7 @@ router.get('/:id', async (req, res) => {
 })
 
 // edit post
-router.put('/dashboard/:id', withAuth, async (req, res)=>{
+router.put('/:id', withAuth, async (req, res)=>{
     try {
         const postData = await Post.update(
             {
@@ -93,7 +93,7 @@ router.put('/dashboard/:id', withAuth, async (req, res)=>{
 })
 
 // delete post
-router.delete('/dashboard/:id', withAuth, async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.destroy({
             where: {
