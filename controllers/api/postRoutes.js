e93031e66ff2dd56774ce0732ff9bc435cb8493f
Keely Bailey <keelybailey3@gmail.com>
2021-09-26 20:49:52 -0500

    Add route to get all posts

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,19 +1,50 @@
 const router = require('express').Router();
-const { Post } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+const sequelize = require('../../config/connection.js')
 
+// new post
 router.post('/', withAuth, async (req, res)=>{
     try {
-        const newPost = await Post.create({
+        const postData = await Post.create({
             ...req.body,
             user_id: req.session.user_id,
         })
-        res.status(200).json(newProject);
+        res.status(200).json(postData);
     } catch (err) {
         res.status(400).json(err)
     }
 })
 
+// get all posts
+router.get('/', async (req, res) => {
+    try {
+        Post.findAll({
+            attributes: ["id", "title", "postCopy", "date_created"],
+            include: [
+                {
+                    model: User, 
+                    attributes: ["name"],
+                }, 
+                {
+                    model: Comment, 
+                    attributes: ["id", "commentContent", "date_created"], 
+                    include: {
+                        model: User,
+                        attributes: ["name"]
+                        }
+                }
+            ],
+        })
+        res.status(200).json(postData);
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// get one post
+
+// edit post
 router.put('/dashboard/:id', withAuth, async (req, res)=>{
     try {
         const postData = await Post.update(
@@ -29,10 +60,11 @@ router.put('/dashboard/:id', withAuth, async (req, res)=>{
         )
         return res.json(postData)
     } catch (err) {
-        return res.json(postData)
+        return res.json(err)
     }
 })
 
+// delete post
 router.delete('/dashboard/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.destroy({
@@ -50,3 +82,5 @@ router.delete('/dashboard/:id', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 })
+
+module.exports = router;
