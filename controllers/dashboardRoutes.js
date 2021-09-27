dc3f2bd45ab5f1a8754ec8e7d2c97b89f8286c79
Keely Bailey <keelybailey3@gmail.com>
2021-09-26 21:33:18 -0500

    Start dashboard route for new route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,35 +1,18 @@
-// const router = require('express').Router();
-// const { Post, User } = require('../models');
-// const withAuth = require('../utils/auth.js');
-// const sequelize = require('../config/connection.js');
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth.js');
+const sequelize = require('../config/connection.js');
 
 // get all posts of user
+router.get('/', withAuth, (req, res) =>{
 
-// router.get('/', withAuth, (req, res) =>{
-       
-//     Post.findAll({
-//         include: [
-//             {
-//                 model: User,
-//                 attributes: ['name'],
-//             },
-//         ],
-//     }).then((postData,err)=>{
-
-//         if(err)res.status(500).json(err);
-    
-
-//         const posts = postData.map((post) => post.get({ plain: true }));
-
-//         res.render('dashboard', {
-//             posts, loggedIn: true
-//         })
-
-// })
-// });
+});
 
 // get one post of user
 
 // create new post
+router.get('/new', withAuth, (req, res)=>{
+    res.render('newPost')
+});
 
-// module.exports = router
+module.exports = router
